e67912f7e25ccf1651a914b5e561ad5de87b2669
Bastian Cournut <bastian.cournut@etu.ec-lyon.fr>
2026-06-06 11:56:47 +0200

    feat:superposition de courbes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 dates = pd.date_range("2026-01-01", "2026-06-06", freq = "D")
 n = len(dates)
 
-price_da = 60 + 14*np.sin(2*np.pi*dates.hour/24) + np.random.normal(0,5,n)
+price_da = 60 + 14*np.sin(2*np.pi*dates.dayofyear/365) + np.random.normal(0,5,n)
 price_id = price_da + np.random.normal(2, 10, n)
 
 df = pd.DataFrame({
@@ -22,17 +22,27 @@ df = pd.DataFrame({
 
 
 app = dash.Dash(__name__)
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df["time"], y = df["price_day_ahead"], name = "Day-Ahead"))
+
+fig1 = go.Figure()
+fig1.add_trace(go.Scatter(x=df["time"], y = df["price_day_ahead"], name = "Day-Ahead"))
+
+fig2 = go.Figure()
+fig2.add_trace(go.Scatter(x=df["time"], y = df["price_intraday"], name = "Intraday"))
+ 
+fig3 = go.Figure()
+fig3.add_trace(go.Scatter(x=df["time"], y = df["price_day_ahead"], name = "Day-Ahead"))
+fig3.add_trace(go.Scatter(x=df["time"], y = df["price_intraday"], name = "Intraday"))
 
 app.layout = html.Div(children=[
     html.H1("Energy Market Dashboard"),
     html.P("Day-Ahead vs Intraday · Europe"),
-    dcc.Graph(id="price-chart", figure=fig) #conteneur du graphe
+    dcc.Graph(id="price-chart1", figure=fig1), #conteneur du graphe
+    dcc.Graph(id="price-chart2", figure=fig2),
+    dcc.Graph(id="price-chart3", figure=fig3)
 ])
 
 
-dcc.Graph(id="price-chart", figure=fig) #conteneur du graphe
+
 
 if __name__ == "__main__":
     app.run(debug=True)
